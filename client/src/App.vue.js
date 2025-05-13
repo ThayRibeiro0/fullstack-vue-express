99d8fede58ed1f7a9b5770b7a255b6aa6767b274
Thays Moia Ribeiro <thaysmoiaribeiro@MacBookPro.lan>
2025-05-12 20:35:13 -0700

    Build set up

---
+++ client/src/App.vue.js
@@ -0,0 +1,35 @@
+"use strict";
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var HelloWorld_vue_1 = require("./components/HelloWorld.vue");
+debugger; /* PartiallyEnd: #3632/scriptSetup.vue */
+var __VLS_ctx = {};
+var __VLS_components;
+var __VLS_directives;
+__VLS_asFunctionalElement(__VLS_intrinsicElements.div, __VLS_intrinsicElements.div)({});
+/** @type {[typeof HelloWorld, ]} */ ;
+// @ts-ignore
+var __VLS_0 = __VLS_asFunctionalComponent(HelloWorld_vue_1.default, new HelloWorld_vue_1.default({}));
+var __VLS_1 = __VLS_0.apply(void 0, __spreadArray([{}], __VLS_functionalComponentArgsRest(__VLS_0), false));
+var __VLS_dollars;
+var __VLS_self = (await Promise.resolve().then(function () { return require('vue'); })).defineComponent({
+    setup: function () {
+        return {
+            HelloWorld: HelloWorld_vue_1.default,
+        };
+    },
+});
+exports.default = (await Promise.resolve().then(function () { return require('vue'); })).defineComponent({
+    setup: function () {
+        return {};
+    },
+});
+; /* PartiallyEnd: #4569/main.vue */
